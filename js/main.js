1ef4760de67fce5e0e6678c025ebc56518178257
David Bador <david.bador@gmail.com>
2020-03-18 15:33:41 +0200

    Made the appending of the marquee children into a function in JS.

--- js/main.js
+++ js/main.js
@@ -18,17 +18,22 @@ createCompanyMarquee = async () => {
     let info = await fetch(`https://financialmodelingprep.com/api/v3/company/stock/list`);
     let data = await info.json();
     data.symbolsList.forEach((option) => {
-        let marqueeChild = document.createElement('div');
-        let marqueeChildPrice = document.createElement('span');
-        marqueeChild.className = 'marqueeChild';
-        marqueeChild.innerHTML = `${option.symbol} `;
-        marqueeChildPrice.className = 'plus';
-        marqueeChildPrice.innerHTML = `$${option.price}`;
-        if (option.exchange === 'Nasdaq Global Select') {
-            marqueeMovement.appendChild(marqueeChild);
-            marqueeChild.appendChild(marqueeChildPrice);
-        }
+        appendStockFeatures(option);
     })
+}
+
+// Function for appending stock elements to the marquee
+appendStockFeatures = (stock) => {
+    let marqueeChild = document.createElement('div');
+    let marqueeChildPrice = document.createElement('span');
+    marqueeChild.className = 'marqueeChild';
+    marqueeChild.innerHTML = `${stock.symbol} `;
+    marqueeChildPrice.className = 'plus';
+    marqueeChildPrice.innerHTML = `$${stock.price}`;
+    if (stock.exchange === 'Nasdaq Global Select') {
+        marqueeMovement.appendChild(marqueeChild);
+        marqueeChild.appendChild(marqueeChildPrice);
+    }
 }
 
 // Asynchronous function for receiving Stock Data
