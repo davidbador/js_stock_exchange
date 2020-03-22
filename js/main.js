b51ce51395f2e57dbcfc1e2ffcdf99283e34062e
David Bador <david.bador@gmail.com>
2020-03-22 10:34:12 +0200

    added if and else statement to createCompanyNames function

--- js/main.js
+++ js/main.js
@@ -38,14 +38,18 @@ appendStockFeatures = (stock) => {
 
 // Asynchronous function for receiving Stock Data
 createCompanyNames = async (x) => {
-    let ticker = await fetch(`https://financialmodelingprep.com/api/v3/search?query=${x}&limit=10&exchange=NASDAQ`);
-    let data = await ticker.json();
-    data.forEach(async (option) => {
-        let info = await fetch(`https://financialmodelingprep.com/api/v3/company/profile/${option.symbol}`);
-        let newData = await info.json();
-        appendStockElement(option, newData.profile);
-    });
-    loader.classList.replace('show', 'hide');
+    if (queryInput.value === '') {
+        return null;
+    } else {
+        let ticker = await fetch(`https://financialmodelingprep.com/api/v3/search?query=${x}&limit=10&exchange=NASDAQ`);
+        let data = await ticker.json();
+        data.forEach(async (option) => {
+            let info = await fetch(`https://financialmodelingprep.com/api/v3/company/profile/${option.symbol}`);
+            let newData = await info.json();
+            appendStockElement(option, newData.profile);
+        });
+        loader.classList.replace('show', 'hide');
+    }
 }
 
 // Function for appending the stock elements to the document
