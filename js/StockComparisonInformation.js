562b729d2daa7f01046e7262b7d65d70e854e91a
David Bador <david.bador@gmail.com>
2020-03-26 15:07:09 +0200

    Completed Milestone 11 HTML, CSS, and JS files

---
+++ js/StockComparisonInformation.js
@@ -0,0 +1,109 @@
+class StockComparisonInformation {
+    constructor(div) {
+        this.div = div;
+    }
+    createComparisonCards = async () => {
+        let urlParams = new URLSearchParams(window.location.search);
+        let symbols = urlParams.get('symbols');
+        let comparisonSymbols = symbols.split(',');
+        comparisonSymbols.forEach(async (option) => {
+            let section = await fetch(`https://financialmodelingprep.com/api/v3/company/profile/${option}`);
+            let data = await section.json();
+            let stockSection = document.createElement('span');
+            stockSection.classList.add('card');
+            stockSection.classList.add('shadow');
+            stockSection.classList.add('p-3');
+            stockSection.classList.add('mb-5');
+            stockSection.classList.add('bg-white');
+            stockSection.classList.add('rounded');
+            this.createComparisonCardsHeaders(data, stockSection);
+            this.createComparisonCardsPrice(data, stockSection);
+            this.createComparisonCardsDescription(data, stockSection);
+            this.createComparisonCardsGraph(option, stockSection);
+        })
+    }
+    createComparisonCardsHeaders = (stock, stockSection) => {
+        let stockSectionTitleParent = document.createElement('div');
+        stockSectionTitleParent.className = 'stockSectionTitleParent';
+        let stockSectionTitleImage = document.createElement('span');
+        let stockSectionTitle = document.createElement('span');
+        let stockSectionTitleIndustry = document.createElement('span');
+        stockSectionTitleImage.innerHTML = `<img src='${stock.profile.image}'/>`;
+        stockSectionTitle.innerHTML = `<a href='${stock.profile.website}'>${stock.profile.companyName}</a>`;
+        stockSectionTitleIndustry.innerHTML = ` (${stock.profile.industry})`;
+        stockSectionTitleIndustry.className = 'stockSectionTitleIndustry';
+        stockSectionTitleParent.appendChild(stockSectionTitleImage);
+        stockSectionTitleParent.appendChild(stockSectionTitle);
+        stockSectionTitleParent.appendChild(stockSectionTitleIndustry);
+        stockSection.appendChild(stockSectionTitleParent);
+        this.div.appendChild(stockSection);
+    }
+    createComparisonCardsPrice = (stock, stockSection) => {
+        let stockSectionPriceParent = document.createElement('div');
+        stockSectionPriceParent.className = 'stockSectionPriceParent';
+        let stockSectionPrice = document.createElement('span');
+        stockSectionPrice.className = 'stockSectionPrice';
+        let stockSectionPriceMovement = document.createElement('span');
+        stockSectionPriceMovement.className = 'stockSectionPriceMovement';
+        stockSectionPrice.innerHTML = `Stock Price: $${stock.profile.price}`;
+        stockSectionPriceMovement.innerHTML = ` ${stock.profile.changesPercentage}`;
+        if (stockSectionPriceMovement.innerText.includes('+')) {
+            stockSectionPriceMovement.classList.add('plus');
+        } else if (stockSectionPriceMovement.innerText.includes('-')) {
+            stockSectionPriceMovement.classList.add('minus');
+        }
+        stockSectionPriceParent.appendChild(stockSectionPrice);
+        stockSectionPriceParent.appendChild(stockSectionPriceMovement);
+        stockSection.appendChild(stockSectionPriceParent);
+    }
+    createComparisonCardsDescription = (stock, stockSection) => {
+        let stockSectionDescription = document.createElement('div');
+        stockSectionDescription.className = 'stockSectionDescription';
+        stockSectionDescription.innerHTML = `${stock.profile.description}`;
+        stockSection.appendChild(stockSectionDescription);
+    }
+    createComparisonCardsGraph = async (option, stockSection) => {
+        let stockSectionGraphParent = document.createElement('div');
+        let stockSectionGraphChild = document.createElement('canvas');
+        let history = await fetch(`https://financialmodelingprep.com/api/v3/historical-price-full/${option}?serietype=line`);
+        let data = await history.json();
+        let dataDates = [];
+        let dataCloses = [];
+        let chosenDataDates = [];
+        let chosenDataCloses = [];
+        data.historical.forEach(function (object) {
+            dataDates.push(object.date);
+            dataCloses.push(object.close);
+        })
+        this.pushData(dataDates, dataCloses, chosenDataDates, chosenDataCloses);
+        var ctx = stockSectionGraphChild.getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: chosenDataDates,
+                datasets: [{
+                    label: 'Stock Price History',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: chosenDataCloses
+                }]
+            },
+            options: {}
+        });
+        stockSectionGraphParent.appendChild(stockSectionGraphChild);
+        stockSection.appendChild(stockSectionGraphParent);
+    }
+    pushData = (dataA, dataB, newDataA, newDataB) => {
+        let val = 18;
+        let divider = Math.floor(dataA.length / val);
+        for (let i = 0; i < dataA.length; i = i + divider) {
+            newDataA.push(dataA[i]);
+        }
+        for (let i = 0; i < dataB.length; i = i + divider) {
+            newDataB.push(dataB[i]);
+        }
+    }
+    loadData = () => {
+        this.createComparisonCards();
+    }
+}
--- js/compare.js
+++ js/compare.js
@@ -1,48 +1,5 @@
-let container = document.getElementById('container');
-
-createComparisonCards = async () => {
-    let urlParams = new URLSearchParams(window.location.search);
-    let symbols = urlParams.get('symbols');
-    let comparisonSymbols = symbols.split(',');
-    comparisonSymbols.forEach(async (option) => {
-        let section = await fetch(`https://financialmodelingprep.com/api/v3/company/profile/${option}`);
-        let data = await section.json();
-        let stockSection = document.createElement('div');
-        stockSection.classList.add('card');
-        stockSection.classList.add('shadow');
-        stockSection.classList.add('p-3');
-        stockSection.classList.add('mb-5');
-        stockSection.classList.add('bg-white');
-        stockSection.classList.add('rounded');
-        createComparisonCardsHeaders(data, stockSection);
-        createComparisonCardsPrice(data, stockSection);
-    })
-}
-
-createComparisonCardsHeaders = (stock, stockSection) => {
-    let stockSectionTitleParent = document.createElement('div');
-    let stockSectionTitleImage = document.createElement('span');
-    let stockSectionTitle = document.createElement('span');
-    let stockSectionTitleIndustry = document.createElement('span');
-    stockSectionTitleImage.innerHTML = `<img src='${stock.profile.image}'/>`;
-    stockSectionTitle.innerHTML = `<a href='${stock.profile.website}'>${stock.profile.companyName}</a>`;
-    stockSectionTitleIndustry.innerHTML = `(${stock.profile.industry})`;
-    stockSectionTitleParent.appendChild(stockSectionTitleImage);
-    stockSectionTitleParent.appendChild(stockSectionTitle);
-    stockSectionTitleParent.appendChild(stockSectionTitleIndustry);
-    stockSection.appendChild(stockSectionTitleParent);
-    container.appendChild(stockSection);
-}
-
-createComparisonCardsPrice = (stock, stockSection) => {
-    let stockSectionPriceParent = document.createElement('div');
-    let stockSectionPrice = document.createElement('span');
-    let stockSectionPriceMovement = document.createElement('span');
-    stockSectionPrice.innerHTML = `Stock Price: $${stock.profile.price}`;
-    stockSectionPriceMovement.innerHTML = `${stock.profile.changesPercentage}`;
-    stockSectionPriceParent.appendChild(stockSectionPrice);
-    stockSectionPriceParent.appendChild(stockSectionPriceMovement);
-    stockSection.appendChild(stockSectionPriceParent);
-}
-
-window.addEventListener('load', createComparisonCards);
+(async function () {
+    let container = document.getElementById('container');
+    const stockInfo = new StockComparisonInformation(container);
+    stockInfo.loadData();
+})()
